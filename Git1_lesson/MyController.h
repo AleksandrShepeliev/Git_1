9e75dec63f2ad23111c5fe0f7568167d5193d592
macbook pro <macbookpro@Yuriis-MacBook-Pro.local>
2017-06-11 11:07:05 +0300

    add my viewController

---
+++ Git1_lesson/MyController.h
@@ -0,0 +1,13 @@
+//
+//  MyController.h
+//  Git1_lesson
+//
+//  Created by macbook pro on 11.06.17.
+//  Copyright © 2017 Shepeliev Aleksandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyController : UIViewController
+
+@end
